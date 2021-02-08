5e031120009959ae58203c192af8f83c25daba28
Harleen Kaur <harleenkr.92@gmail.com>
2021-02-08 14:40:17 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import numpy as np
 
 import os
 import joblib
+import base64
 
 # Data viz packages
 
@@ -16,37 +17,52 @@ import matplotlib.pyplot as plt
 import matplotlib
 import pandas as pd
 from plot_tsne_py import PlotTSNE
+from generate import generate
+from generate import Generator
+from generate import Discriminator
 
 # matplotlib.use('Agg')
+
+hide_streamlit_style = """ <style> #MainMenu {visibility: hidden;} footer {visibility: hidden;} </style> """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# File Download
+
+def make_downloadable(df, format_type='csv'):
+    if format_type == 'csv':
+        datafile = df.to_csv(index = 'False')
+    elif format_type =='json':
+        datafile = df.to_json()
+
+    b64 = base64.b64encode(datafile.encode()).decode()
+    href = f'<a href="data:file/{format_type};base64,{b64}" download="myfile.csv">Download file</a>'
+    st.markdown(href, unsafe_allow_html=True)
+
 
 def main():
     """ TB DASHBOARD """
 
     st.title("TB DASHBOARD")
 
-    menu = ["Home", "Login", "Signup"]
-    choice1 = ["Metrics1", "Metrics2"]
-    choice2 = ["Model1", "Model2"]
+    #menu = ["Home", "Login", "Signup"]
+    #choice1 = ["Metrics1", "Metrics2"]
+    choice1 = ["Select model", "wgan", "gmm"]
+    choice2 = ["Select datatype", "TB", "healthy"]
 
-    # Dropdowns
-    algo = st.sidebar.selectbox("Select Algorithm", ("Algo1", "Algo2", "Algo3", "Algo4", "Algo5"))
-    metrics = st.sidebar.selectbox("Choose Metrics", choice1)
-    activity = st.sidebar.selectbox("Choose pre-trained models", choice2)
+    # Data generation
 
-    # Text input 
-    text = st.sidebar.text_input('Input your genes here:')
+    models = st.sidebar.selectbox("Choose pre-trained models", choice1)
+    datatype = st.sidebar.selectbox("Choose pre-trained models", choice2)
+    number = st.sidebar.number_input("Number", 10, 1000)
+    dataformat = st.sidebar.selectbox("Save As", ['csv', 'json'])
 
-    # File uploader
-    data_file = st.file_uploader("Upload CSV", type = ["csv"])
+    if st.sidebar.button("Generate Samples"):
+        df = generate(models, number, datatype)
+        st.dataframe(df)
+        make_downloadable(df, dataformat)
 
-    if st.button("Process"):
-        if data_file is not None:
-            st.write(type(data_file))
-            #df = pd.read_csv(data_file)
-            #st.dataframe(df)
-            rd = PlotTSNE(data_file, "/Users/mypc/Desktop/streamlit/gpbt2Axes_1000.csv")
-            rd.plot_tsne()
-
+    # Text input
+    #text = st.sidebar.text_input('Input your genes here:')
 
 
 if __name__ == '__main__':
